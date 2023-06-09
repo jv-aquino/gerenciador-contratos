a914ee33e2e980d0698150b394f5b0dbb12acc30
jv-aquino <jvaquinocontact@gmail.com>
2023-06-08 21:10:32 -0300

    + start building ui

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className="px-6 py-7 bg-dark-blue-700">
+      
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  )
+}
