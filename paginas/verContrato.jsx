3c2299ad78fb99eae620c1961d22caa7aaa991c7
jv-aquino <jvaquinocontact@gmail.com>
2023-07-31 10:06:49 -0300

    + add print relatorio

--- paginas/verContrato.jsx
+++ paginas/verContrato.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import supabase from "@/lib/supabase";
+import ReactToPrint from "react-to-print";
 
 import Carregando from '@/components/Carregando';
 
 export default function VerContrato({ id }) {
+  const relatorio = useRef();
+
   const [loading, setLoading] = useState(true);
   const [contrato, setContrato] = useState({});
 
@@ -35,9 +38,20 @@ export default function VerContrato({ id }) {
     <>
       <h1>Contrato #{contrato.id}</h1>
 
-      <div className="content flex flex-col">
+      <div className="content flex flex-col relative print-contrato" ref={relatorio}>
+      <ReactToPrint
+          trigger={() => { return (
+          <button className="print-button p-3">
+            <span className="symbol text-2xl">print</span>
+          </button>
+          )
+        }}
+          content={() => relatorio.current}
+        />
+
         <h2 className="font-semibold text-3xl self-center">{contrato["Objeto"]}</h2>
         <h3 className="font-medium text-2xl italic pr-1.5 text-gray-400 self-center">{contrato["Tipo"]}</h3>
+
         <p>Processo: {contrato["Processo"]}</p>
         <p>Número do contrato: {contrato["Numero_contrato"]}</p>
         <p>Fim do contrato: {contrato["Vigencia_final"]}</p>
@@ -47,7 +61,9 @@ export default function VerContrato({ id }) {
         <br/>
         <p>Dias até o fim da vigência: <span className={"font-semibold " + ((contrato["Dias_Restantes"] <= 30) ? "text-red-700" : ((contrato["Dias_Restantes"] <= 90) ? "text-amber-600" : ""))}>
             {(contrato["Dias_Restantes"] < 0) ? "Expirado" : contrato["Dias_Restantes"]}
-          </span>
+        </span>
+
+        {/* menu de responsáveis pelo contrato */}
         </p>
       </div>
     </>
