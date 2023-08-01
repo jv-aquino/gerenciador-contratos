0e8a631775900630345fb517aa941a70f6ba2481
jv-aquino <jvaquinocontact@gmail.com>
2023-08-01 08:29:55 -0300

    + update login

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
 import Inicio from '@/paginas/inicio'
-import Login from '@/paginas/login'
+import Login from '@/app/login'
 
 import Contratos from '@/paginas/contratos'
 import Inserir from '@/paginas/inserirContrato'
